3bfbce9662ebf784cd333e15f01f2b5dfcd69d52
TerynD <tldunnham23@gmail.com>
2020-03-11 14:46:01 -0600

    jsless22

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("Welcome, user!");
+let a = prompt("value 1", " ");
+let b = prompt("value 2", " ");
+
+if (a > b) message = "The number is greater";
+else if (a < b) message = "The number is less";
+else if (a == b) message = "The number is equal";
+
+alert(message);
